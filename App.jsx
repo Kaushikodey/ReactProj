ebd9cc49954ffda31cc83a90c5212f7faea82957
Kaushik Dey <kaushikdey59@gmail.com>
2026-01-31 19:57:07 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,3 +1,18 @@
-<button onClick={() => navigate('/plants')}>
-  Get Started
-</button>
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const App = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="background-image">
+      <h1>Welcome to Paradise Nursery</h1>
+
+      <button onClick={() => navigate("/plants")}>
+        Get Started
+      </button>
+    </div>
+  );
+};
+
+export default App;
